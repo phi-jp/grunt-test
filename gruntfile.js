bcea5fc65701eb800f0af38ee29ab4866b4aa24a
phi <phi1618jp@gmail.com>
2013-03-17 02:01:54 +0900

    fix

--- gruntfile.js
+++ gruntfile.js
@@ -1,4 +1,8 @@
 module.exports = function(grunt) {
+  grunt.loadNpmTasks('grunt-contrib-coffee');
+
+	this.pageTitle = 'hoge';
+
   grunt.initConfig({
     coffee: {
       app: {
@@ -29,6 +33,25 @@ module.exports = function(grunt) {
 			  src: ['css/test.css'],
 				dest: 'build/test.min.css'
 			}
+		},
+		jade: {
+      my_target: {
+			  files: {
+				  'html/index.html': ['jade/index.jade']
+				},
+				options: {
+				  locals: {
+					  'pageTitle': 'Grunt.js test'
+					}
+				}
+			}
+		},
+		htmlmin: {
+		  my_target: {
+			  files: {
+				  'build/index.html': 'html/index.html'
+				}
+			}
 		}
   });
   
@@ -36,6 +59,8 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-less');
   grunt.loadNpmTasks('grunt-contrib-coffee');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-jade');
+  grunt.loadNpmTasks('grunt-contrib-htmlmin');
 
-  grunt.registerTask('test', ['coffee', 'uglify', 'less', 'cssmin']);
+  grunt.registerTask('test', ['coffee', 'uglify', 'less', 'cssmin', 'jade', 'htmlmin']);
 }
