7bb4e60af753c20ca585113812f160401f8bc668
phi <phi1618jp@gmail.com>
2013-03-17 01:28:11 +0900

    fix

--- gruntfile.js
+++ gruntfile.js
@@ -12,17 +12,30 @@ module.exports = function(grunt) {
       my_target: {
         options: {
         
-	    },
+	      },
         files: {
           'build/test.min.js': [ 'js/test.js' ]
         },
       },
     },
+		less: {
+		  my_target: {
+			  src: ['less/test.less'],
+				dest: 'css/test.css'
+			}
+		},
+		cssmin: {
+		  my_target: {
+			  src: ['css/test.css'],
+				dest: 'build/test.min.css'
+			}
+		}
   });
   
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-less');
   grunt.loadNpmTasks('grunt-contrib-coffee');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
 
-  grunt.registerTask('test', ['coffee', 'uglify']);
+  grunt.registerTask('test', ['coffee', 'uglify', 'less', 'cssmin']);
 }
